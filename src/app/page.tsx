1a17eb727c8e4ac250c2296d4e9f10ddd635c593
Desmond Chai <66651820+dezchai@users.noreply.github.com>
2023-05-07 20:49:50 -0700

    added favicon and html metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,14 @@ import SearchBar from "../components/SearchBar";
 
 function App() {
   return (
-    <>
+    <div>
+      <h1 className="font-roboto text-center text-5xl font-bold m-6">
+        Teacher Salary
+      </h1>
       <div>
-        <h1 className="font-roboto text-center text-5xl font-bold m-6">
-          Teacher Salary
-        </h1>
-        <div>
-          <SearchBar />
-        </div>
+        <SearchBar />
       </div>
-    </>
+    </div>
   );
 }
 
