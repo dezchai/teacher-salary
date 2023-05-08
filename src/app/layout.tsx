a75b61394c2778912bd7d64a815b93ac9dcee6fc
Desmond Chai <66651820+dezchai@users.noreply.github.com>
2023-05-07 21:09:12 -0700

    favicon?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 export const metadata = {
   title: 'Teacher Salary',
   description: 'Find the salary of almost any teacher in British Columbia!',
+
 }
 
 export default function RootLayout({
@@ -11,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <link rel="icon" href="/favicon.ico" />
     </html>
   )
 }
