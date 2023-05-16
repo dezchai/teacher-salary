c65591eb8988c68955bc18e788a27ebff388fbf0
teever <cho.trevor2@gmail.com>
2023-05-16 01:21:36 -0700

    fix footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-import Footer from "@/components/Footer";
-import { Providers } from "./providers";
+import Footer from '@/components/Footer'
+import { Providers } from './providers'
 
 export const metadata = {
-  title: "Teacher Salary",
-  description: "Find the salary of almost any teacher in British Columbia!",
-};
+  title: 'Teacher Salary',
+  description: 'Find the salary of almost any teacher in British Columbia!'
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
-      <div className="flex flex-col min-h-screen">
-        <main className="flex-grow">
-          {/* NEXTJS WILL NOT COMPILE WITHOUT BODY TAG */}
-          <Providers>{children}</Providers>
-          <body className="hidden"></body>
-        </main>
-        <Footer />
-      </div>
+    <html lang='en'>
+      <link
+        rel='icon'
+        href='/favicon.ico'
+      />
+      <body>
+        <Providers>
+          <div className='flex flex-col justify-between min-h-screen'>
+            {children}
+            <Footer />
+          </div>
+        </Providers>
+      </body>
     </html>
-  );
+  )
 }
