11e156902fe301d475e1ca863f9bd461b1f36bbf
Desmond Chai <66651820+dezchai@users.noreply.github.com>
2023-05-16 16:28:31 -0700

    added about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,59 +1,57 @@
-import React from "react";
-
 const About = () => {
   return (
-    <div>coming soon</div>
-    // <div className="text-xl p-4 mx-auto min-w-xl max-w-4xl">
-    //   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia
-    //   maximus metus eu pretium. Vivamus fermentum dapibus commodo. Proin viverra
-    //   libero quis volutpat suscipit. Pellentesque habitant morbi tristique
-    //   senectus et netus et malesuada fames ac turpis egestas. Morbi sit amet
-    //   ligula ipsum. Proin scelerisque leo sem, a auctor dui euismod id.
-    //   Phasellus tincidunt libero a orci cursus, ut aliquet neque convallis. Nunc
-    //   blandit suscipit turpis, sit amet bibendum dolor finibus sed. Integer odio
-    //   est, feugiat a venenatis eget, commodo sit amet urna. Etiam et eros
-    //   blandit quam elementum scelerisque vel nec metus. Nulla ut imperdiet
-    //   mauris, sit amet venenatis purus. Mauris nisl sapien, molestie eu mi ac,
-    //   scelerisque aliquam justo. Fusce iaculis cursus nisl, vel convallis arcu
-    //   pretium quis. Nunc aliquam fringilla massa, ac aliquet enim condimentum
-    //   non. Duis congue nulla est. Quisque tincidunt diam eu orci mattis
-    //   ullamcorper. Nullam tincidunt ante et enim tincidunt consectetur. Nulla
-    //   mattis vel erat non lobortis. Vivamus sed magna venenatis, posuere erat
-    //   in, scelerisque dolor. Proin luctus consectetur quam, ut pharetra sem
-    //   dapibus sit amet. Donec ut lacus et diam lacinia blandit. Proin metus
-    //   massa, luctus vitae porttitor ut, pellentesque nec nunc. Praesent quis
-    //   turpis diam. Phasellus ultrices pulvinar sem, ac mattis risus gravida sed.
-    //   In eget aliquet lorem. Vestibulum laoreet vitae elit vel lobortis. Nam vel
-    //   tempus enim. Donec vitae sodales nibh. Sed ornare sollicitudin neque, sit
-    //   amet porttitor risus malesuada at. Vestibulum iaculis vehicula convallis.
-    //   In a vulputate quam. Aliquam vitae metus varius leo pulvinar malesuada
-    //   eget ac justo. Nulla molestie ut turpis et molestie. Sed pellentesque
-    //   porta ullamcorper. Phasellus ullamcorper sodales viverra. Donec auctor
-    //   luctus arcu, nec bibendum purus fermentum ac. Donec sit amet tristique
-    //   mauris. Morbi rutrum, nunc in maximus lacinia, arcu mauris condimentum
-    //   urna, non feugiat nisi massa iaculis tellus. Phasellus facilisis faucibus
-    //   eros, nec imperdiet enim malesuada lobortis. Donec at ante ac sem
-    //   tincidunt pretium. In pharetra euismod risus, eget ullamcorper lorem.
-    //   Donec sit amet lacus venenatis, porttitor lacus nec, ultrices nunc. Cras
-    //   molestie gravida sapien nec tempor. Etiam vitae elit porta, egestas nisl
-    //   quis, ultrices felis. Nullam sed posuere velit, nec faucibus orci. Sed in
-    //   auctor purus, fermentum tincidunt ligula. Integer eu rutrum sem. Ut
-    //   feugiat ac arcu sit amet laoreet. Aliquam erat volutpat. Vivamus in
-    //   rhoncus diam, eget ultricies massa. Praesent sagittis mollis hendrerit.
-    //   Suspendisse dictum dictum tincidunt. Curabitur tempus eget enim nec
-    //   posuere. In molestie libero a condimentum convallis. Curabitur placerat
-    //   est augue, a euismod ante bibendum eget. Fusce elementum dolor auctor,
-    //   sagittis urna quis, porta libero. Proin commodo ultrices aliquet.
-    //   Pellentesque efficitur urna id metus placerat ultrices. Duis pellentesque,
-    //   libero sit amet porttitor hendrerit, magna nibh placerat elit, ut
-    //   ullamcorper nisl urna laoreet lacus. Cras eu quam nunc. Aenean consectetur
-    //   lacinia felis, vitae finibus velit tincidunt ut. Donec pharetra, augue
-    //   quis sollicitudin ultricies, erat nisi tincidunt nisl, in accumsan nisi
-    //   dolor vitae quam. Nulla sollicitudin faucibus turpis ac fringilla. Duis
-    //   scelerisque ipsum id ex vehicula viverra. Donec nisi massa, suscipit quis
-    //   semper sit amet, lobortis id tellus. Generated 5 paragraphs, 493 words,
-    //   3382 bytes of Lorem Ipsum
-    // </div>
+    <div className="">
+      <h2 className="text-3xl font-bold">About</h2>
+      <p className="text-xl mx-auto min-w-xl max-w-4xl">
+        Ever wanted to know how much your teacher makes? Well, now you can! This
+        website allows you to search for teachers in British Columbia and see
+        how much they make. The data comes from financial statements published
+        by universities and school districts.
+      </p>
+      <h2 className="text-3xl font-bold pt-5">I couldn’t find my teacher!</h2>
+      <p className="text-xl mx-auto min-w-xl max-w-4xl">
+        The BC Financial Information Act only requires that employees that make
+        more than $75,000 be published. However, some districts/universities may
+        contain outdated data. This will be fixed in an upcoming update.
+      </p>
+      <h2 className="text-3xl font-bold pt-5">Technical Details</h2>
+      <p className="text-xl mx-auto min-w-xl max-w-4xl">
+        Many different methods were used to parse the data in the respective
+        PDFs. Initially, I tried to parse the tables with Tabula but quickly
+        found that it didn’t work all that well. It required a lot of manual
+        adjusting, which was tedious to do. I contacted Nathan Griffiths from
+        the Vancouver Sun as they have completed{" "}
+        <a
+          href="https://vancouversun.com/news/local-news/bc-public-sector-salaries-database-sunshine-list"
+          target="blank"
+        >
+          something similar
+        </a>
+        Their database covers all public servants but also contains outdated
+        data for certain districts/institutions. Nathan had told me that from
+        his experience, there was no way around manual cleanup when using Tabula
+        or Adobe Acrobat.
+      </p>
+      <p className="text-xl mx-auto min-w-xl max-w-4xl pt-5">
+        Through trial and error, I discovered an optimal data parsing method. It
+        involved copying all the data as plaintext through a PDF viewer and
+        cleaning it with Python. This was much better than Tabula because Tabula
+        would occasionally miss some aspects if formatted strangely. By avoiding
+        Tabula, I could ensure I would not miss any data, plus the cleaning was
+        a lot easier. The database is mainly filled with data I parsed, but I
+        used the Vancouver Sun’s data for a few districts. Eventually, the
+        database will soon contain data entirely collected by me.
+      </p>
+      <p className="text-xl mx-auto min-w-xl max-w-4xl pt-5">
+        The front end of this site was initially built using vanilla React with
+        an Express.js backend and deployed on AWS Elastic Beanstalk. However,
+        the current deployment is created with Next.js and deployed on Vercel
+        since Next.js v13 makes everything more manageable (and it’s free). The
+        backend uses the fuzzy npm module, which means misspelled queries should
+        still work! The website includes a responsive design with Tailwind CSS
+        and supports mobile devices.
+      </p>
+    </div>
   );
 };
 
