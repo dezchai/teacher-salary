e818448012e6208b4c41c1d47680f0d5ec05b1f8
Desmond Chai <66651820+dezchai@users.noreply.github.com>
2023-05-16 17:31:51 -0700

    added viewer analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
 import "./globals.css";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
             <Header />
             {children}
           </div>
+          <Analytics />
           <Footer />
         </Providers>
       </body>
